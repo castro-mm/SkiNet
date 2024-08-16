03880d7b0e21137831b7e9971ed75e36a1d37268
Marcelo Castro <marcelo.castro@autotrac.com.br>
2024-08-16 18:28:30 -0300

    End of Lecture 23 of Section 3

--- Api/Program.cs
+++ Api/Program.cs
@@ -30,4 +30,20 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+// Seed data
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
+
 app.Run();
