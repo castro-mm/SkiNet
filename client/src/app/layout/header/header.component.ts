40f90c5c199ed23b3b7a6072b4fbe3885f17bc38
Marcelo Moura de Castro <castro.mm@yahoo.com>
2024-08-24 10:21:30 -0300

    End of Section 11

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
     selector: 'app-header',
@@ -14,10 +16,12 @@ import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
         RouterLink,
         RouterLinkActive,
         RouterModule,
+        MatProgressBar
     ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+    constructor (public busyService: BusyService) { }
 
 }
